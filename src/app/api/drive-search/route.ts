77176b98236aa71c832ad77e21ed7cf6da03de99
openhan <LOREEN@OPENHAN.KR>
2026-06-23 11:25:18 +0900

    update

--- src/app/api/drive-search/route.ts
+++ src/app/api/drive-search/route.ts
@@ -43,7 +43,6 @@ export async function GET(req: NextRequest) {
     orderBy: "modifiedTime desc",
     supportsAllDrives: "true",
     includeItemsFromAllDrives: "true",
-    corpora: "allDrives",
   });
 
   try {
